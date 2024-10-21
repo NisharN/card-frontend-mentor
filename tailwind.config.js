66101aa382f3d817c7cc2c0adfdb4f3b3c84261a
NisharN <nisharr03@gmail.com>
2024-10-21 10:16:25 +0530

    new commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      screens: {
+        sm:'375px',
+      }
+    },
+  },
+  plugins: [],
+}
+
